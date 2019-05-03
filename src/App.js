7536dc2208d19e15d53a5154654844f43608ac83
h2oche <h2oche22@gmail.com>
2019-05-03 16:40:49 +0900

    add deploy.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          TESTTEST
+          123123
         </p>
         <a
           className="App-link"
