e85fe774cd89f6fd4cd6676124d7512ba4eabaae
h2oche <h2oche22@gmail.com>
2019-05-05 00:57:34 +0900

    working with Board.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ import NoticeBoard from "./Notices/BoardList";
 import Settings from "./Settings/Settings";
 import Board from './Notices/Board';
 import AddBoard from "./Notices/AddBoard";
+import Notice from './Notices/Notice';
+import AddNotice from './Notices/AddNotice';
 
 function App() {
   return (
@@ -21,7 +23,9 @@ function App() {
         <Route exact path="/BOBO/classRecord" component={ClassRecord}/>
         <Route exact path="/BOBO/addBoard" component={AddBoard}/>
         <Route exact path="/BOBO/board" component={NoticeBoard}/>
-        <Route path="/BOBO/board/:id" component={Board}/>
+        <Route exact path="/BOBO/board/:id/addNotice" component={AddNotice}/>
+        <Route exact path="/BOBO/board/:id" component={Board}/>
+        <Route path="/BOBO/notice/:id" component={Notice}/>
         <Route exact path="/BOBO/settings" component={Settings}/>
         <Menu/>
       </div>
