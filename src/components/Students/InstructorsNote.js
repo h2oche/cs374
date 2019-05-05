680cb02c10a5a59608a84169a93dd4b2c7dfb769
Sangjin Yun <tommy@Tommys-MacBookPro.local>
2019-05-05 23:42:13 +0900

    InstructorsNote UI Complete

--- src/components/Students/InstructorsNote.js
+++ src/components/Students/InstructorsNote.js
@@ -5,50 +5,58 @@ import '../../css/Students/InstructorsNote.css'
 import {Redirect} from 'react-router';
 import Demographic from './Demographic'
 import Popup from 'reactjs-popup'
-import {Link} from 'react-router-dom'
 import {Checkbox} from 'react-materialize'
+import {Link} from 'react-router-dom'
 
 class DeletePopup extends Component {
-    state={
-        checked:false
-    }
+  state={
+    checked:false,
+    open:false
+  }
 
-    check =(event)=> {
-        console.log('pressed');
-        var a = !this.state.checked;
-        this.setState({checked:a});
-        console.log(this.state.checked);
-        this.forceUpdate();
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Popup contentStyle={{width: '80%'}} trigger={<button>Delete</button>} position="top right">
-              {close => (
-                  <div className="NoteDeleteContainer">
-                      <span className="NoteDeleteTitle">
-                          Delete Note
-                      </span>
-                      <br/>
-                      {/*<input type="checkbox" onChange={this.checked} checked={this.state.checked}></input>*/}
-                      {this.state.checked? (<Checkbox className = "NoteDeleteCheckbox" onChange={this.check} checked/>) : 
-                          (<Checkbox className = "NoteDeleteCheckbox" onChange={this.check}/>)}
-                      <span className="NoteDeleteWarningText">I hereby understand that I can not recover the deleted note.</span>
-                      <br/>
-                      <br/>
-                      <button >No</button>
-                      {this.state.checked? (
-                      <Link to={"/BOBO/studentProfile/instructorsNote/"+this.props.instructor+'/' +this.props.student+'/'}>
-                          <button className="close pinkcancelbutton" style={{float:"right"}} onClick={close+this.check}>
-                              Yes
-                          </button>
-                      </Link>) : null}
-                  </div>
-                )
-              }
-            </Popup>
-        )
-    }
+  check =(event)=> {
+    this.setState({checked: !this.state.checked});
+  }
+
+
+  deleteNote = () => {
+    //TODO: delete on firebase
+    window.location.reload();
+  }
+
+
+  render() {
+    return (
+      <Popup contentStyle={{width: '80%'}} trigger={<button>Delete</button>} position="top right">
+        { close => (
+          <div className="NoteDeleteContainer">
+              <span className="NoteDeleteTitle">
+                  Delete Note
+              </span>
+              <br/>
+                <Checkbox className = "NoteDeleteCheckbox" onChange={this.check} label="" value=""/>
+              <span className="NoteDeleteWarningText">I hereby understand that I </span>
+              <span className="NoteDeleteWarningTextImportant">cannot recover</span>
+              <span className="NoteDeleteWarningText"> the deleted note.</span>
+              <br/>
+              <br/>
+                <button className="close pinkcancelbutton" onClick={close}>
+                    No
+                </button>
+              {this.state.checked? (
+                <button className="close pinkcancelbutton" style={{float:"right"}} onClick={this.deleteNote}>
+                    Yes
+                </button>
+              ) : null}
+          </div>)
+        }
+      </Popup>
+    )
+  }
 }
 
 export class InstructorsNote extends Component {
@@ -84,20 +92,24 @@ export class InstructorsNote extends Component {
 
     renderNoteList = () => {
         var  myNoteExists = false;
+        var i =0;
         var noteList = this.state.Notes.map(_note => {
-            if(_note.Instructor!==this.props.match.params.id)
-                return <Note data={_note}/>
+            if(_note.Instructor!==this.props.match.params.instructor_id)
+                return <Note data={_note} key={i++}/>
             else
             {
                 myNoteExists = true;
-                return <MyNote data={_note}/>
+                return <MyNote data={_note} key={i++}/>
             }
         });
 
         if(!myNoteExists)
             noteList.push(
                 <div className="AddButtonContainer">
+                  <Link to={"/BOBO/studentProfile/instructorsNoteAddModify/"+this.props.match.params.instructor_id+
+                              '/'+this.props.match.params.student_id}>
                     <button className="AddButton">Add Note!</button>
+                  </Link>
                 </div>
             );
         return <div className="NoteContainer">{noteList}</div>;
@@ -109,7 +121,8 @@ export class InstructorsNote extends Component {
         return (
         <div style = {{width:"100%"}} className="content InstructorsNoteContent">
             <div>
-                <Topbar name="Instructor's Note" showBack={true} backTo = {"/BOBO/studentProfile/main/"+this.props.match.params.id}></Topbar>
+                <Topbar name="Instructor's Note" showBack={true} backTo = {"/BOBO/studentProfile/main/"+
+                                  this.props.match.params.instructor_id+'/'+this.props.match.params.student_id}></Topbar>
             </div>
             <hr style = {{width: "100%", border:'none', backgroundColor:'darkgray', height:'2px'}}/>
             <Demographic />
@@ -138,8 +151,11 @@ class MyNote extends Component {
             <div>
                 <div className="Note">
                     <div className="MyNoteButton">
+                      <Link to={"/BOBO/studentProfile/instructorsNoteAddModify/"+this.props.data.Instructor+
+                              '/'+this.props.data.Student}>
                         <button>Modify</button>
-                        <DeletePopup instructor={this.props.data.Instructor} student={this.props.data.Student}/>
+                      </Link>
+                      <DeletePopup instructor_id={this.props.data.Instructor} student_id={this.props.data.Student}/>
                     </div>
                     <span className="NoteInstructor">{'• '+this.props.data.Instructor+'\'s Note'}</span>
                     <br/>
