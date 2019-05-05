4a21c456b6059201dc3ccbf017a8d7e5191bf038
박윤하 <yunha@bag-yunhaui-MacBook-Pro.local>
2019-05-06 04:37:33 +0900

    menu design

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import { Icon } from 'react-materialize';
+
 import "../css/Menu.css";
 
 export class Menu extends Component {
   render() {
     return (
       <div className="menu">
-        <div className="menu-item"><Link to="/BOBO">Student List</Link></div>
-        <div className="menu-item"><Link to="/BOBO/classRecord">Class Record</Link></div>
-        <div className="menu-item"><Link to="/BOBO/board">Notice</Link></div>
-        <div className="menu-item"><Link to="/BOBO/settings">Settings</Link></div>
+        <div className="menu-item"><Link to="/BOBO">
+          <Icon className="menu-icon" medium='true'>assignment_ind</Icon>
+          </Link></div>
+        <div className="menu-item"><Link to="/BOBO/classRecord">
+          <Icon className="menu-icon" medium='true'>add_box</Icon>
+          </Link></div>
+        <div className="menu-item"><Link to="/BOBO/board">
+          <Icon className="menu-icon" medium='true'>assignment</Icon>
+          </Link></div>
+        <div className="menu-item"><Link to="/BOBO/settings">
+          <Icon className="menu-icon" medium='true'>settings</Icon>
+          </Link></div>
       </div>
     )
   }
