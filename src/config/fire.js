180ede184db2d6c6d6d712467802b5bcaf6358f0
KimJungwoo <jw950310@kaist.ac.kr>
2019-05-06 20:41:32 +0900

    upload_file try 3

--- src/config/fire.js
+++ src/config/fire.js
@@ -78,7 +78,35 @@ export const updateChild = (dir, childName, value) => {
 }
 
   
-export const upload_file = (file) => {
-  let target = storage.ref('images/' + new Date());
-  target.put(file);
+export const upload_file = (dir, file, filename) => {
+  let target = storage.ref(dir + filename);
+  //target.put(file);
+  var uploadTask = target.put(file);
+
+  // Register three observers:
+  // 1. 'state_changed' observer, called any time the state changes
+  // 2. Error observer, called on failure
+  // 3. Completion observer, called on successful completion
+  uploadTask.on('state_changed', function(snapshot){
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+    // Handle unsuccessful uploads
+  }, function() {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      console.log('File available at', downloadURL);
+    });
+  });
 }
