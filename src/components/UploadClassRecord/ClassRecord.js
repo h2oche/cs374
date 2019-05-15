c8eb7fe6e2b2a7105865adaa10ddb6c8104163c7
KimJungwoo <jw950310@kaist.ac.kr>
2019-05-15 18:43:51 +0900

    cute try (not usable yet)

--- src/components/UploadClassRecord/ClassRecord.js
+++ src/components/UploadClassRecord/ClassRecord.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Row, Col, Autocomplete,Button, Textarea,Checkbox} from 'react-materialize';
+import {Row, Col, Autocomplete,Button,TextInput,Icon, Textarea,Checkbox} from 'react-materialize';
 import Topbar from "../Topbar";
 import '../../css/ClassRecord.css';
 import '../../css/Common.css';
@@ -43,6 +43,7 @@ const INITIAL_STATE = {
   redirectTo: "",
   disabled: false,
   Hashtag:[],
+  showmodifiy:false,
 
 };
 
@@ -80,11 +81,16 @@ export class ClassRecord extends Component {
   
   handlenameChange = e =>
   {
+    
     this.setState({...this.state, Studentname : e.target.value});
   }
   handlecontentChange = e =>
   {
+    console.log(e.target);
     this.setState({...this.state, Text : e.target.value});
+    var select_parent = document.getElementById("studentname");
+    console.log(this.state.Studentname);
+    select_parent.value = this.state.Studentname;
   }
   handlefileChange(e) {
   this.setState({...this.state, file:e.target.files});
@@ -93,15 +99,17 @@ export class ClassRecord extends Component {
     console.log(this.state.Users);
 
     var parent = this.state.Users.find(_user => _user.name === _userName);
+    
+    this.setState({...this.state, StudentID:parent.id,Studentname:_userName, showmodifiy:true});  
+    var aftername = document.getElementById("studentname");
+    aftername.value = _userName;
+    /*this.setState({...this.state, StudentID:parent.id,Studentname:_userName, showAutocomplete:false, showmodifiy:true});  
 
-    this.setState({...this.state, StudentID:parent.id,Studentname:_userName});
-    var select_parent = document.getElementById("studentname");
-    select_parent.value = _userName;
-
+    var aftername = document.getElementById("after");
+    aftername.value = _userName;*/
   }
   doneonClick = () =>
   {
-    //console.log(this.state);
     var obj = {...this.state};
     var rawdate = new Date();
     var rawmonth = rawdate.getMonth() + 1;
@@ -121,20 +129,14 @@ export class ClassRecord extends Component {
     obj.Hashtag = [];
 
     while ((match = regex.exec(obj.Text))) {
-        // matches.push(match[1]);
         obj.Hashtag.push(match[1]);
     }
-
-    // console.log(matches);
 
     if(obj.file)
     {
       var fromfile = document.getElementById("inputfile");
       var fromcamera = document.getElementById("inputcamera");
       var fromgallery = document.getElementById("inputgallery");
-
-      console.log("123123"+fromfile.files);
-      
       
       Array.prototype.forEach.call(fromfile.files, function(file) { 
         upload_file('images/',file,file.name);
@@ -145,10 +147,6 @@ export class ClassRecord extends Component {
        Array.prototype.forEach.call(fromgallery.files, function(file) { 
         upload_file('images/',file,file.name);
        });
-
-      
-      //upload_file('images/', x.files[0], x.files[0].name);
-      //upload_file('images/', x.files[0], x.files[0].name);
     }
     pushDB("Record", obj)
     .then(_res => {
@@ -156,6 +154,13 @@ export class ClassRecord extends Component {
       this.inputElementname.value="";
       this.setState({...this.state,/*StudentID: ,*/ redirect: true, redirectTo: "/studentList/tommy11" });
     });
+  }
+  
+  flipbool = () => {
+    if(this.state.showAutocomplete)
+      this.setState({...this.state, showAutocomplete:false});
+    else
+      this.setState({...this.state, showAutocomplete:true});
   }
 
   fileclick = () => {
@@ -186,8 +191,10 @@ export class ClassRecord extends Component {
                 options={{data: this.state.autocompleteData, onAutocomplete:this.onAutocomplete}}
                 placeholder="Search student name"
                 icon="search" s={12}/> :
-              <span></span>
+                <span></span>
+                //<Textarea onChange={this.flipbool} id="after" value="" icon="search" s={12}/>       
             }
+
           </Col>
         </Row>
         <div className="buttons">
